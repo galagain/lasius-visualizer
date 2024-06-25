b9b7fdce4cfec6e6233515682504cacae6a1220c
galagain <calvin@galagain.com>
2024-06-25 14:43:11 +0200

    feat: add button to sort papers by citation count or date

--- public/click_handler.js
+++ public/click_handler.js
@@ -1,19 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
     const titleList = document.getElementById('title-list');
+    const sortButton = document.getElementById('sort-button');
+    let sortByCitations = true; // Default sorting by citations
 
     document.getElementById('button-container').addEventListener('click', (event) => {
         if (event.target.tagName === 'BUTTON' && event.target.className === 'paper-button') {
             try {
                 const jsonData = JSON.parse(event.target.dataset.json);
-                displayPaperTitles(jsonData.nodes);
+                displayPaperTitles(jsonData.nodes, sortByCitations);
             } catch (error) {
                 alert('Error displaying paper titles.');
             }
         }
     });
 
-    function displayPaperTitles(nodes) {
-        nodes.sort((a, b) => b.citationCount - a.citationCount);
+    sortButton.addEventListener('click', () => {
+        sortByCitations = !sortByCitations; // Toggle sorting criteria
+        sortButton.textContent = sortByCitations ? 'Sort by Date' : 'Sort by Citations';
+        const event = new CustomEvent('sortPapers', { detail: { sortByCitations } });
+        document.dispatchEvent(event);
+    });
+
+    document.addEventListener('sortPapers', (event) => {
+        const jsonData = JSON.parse(document.querySelector('.paper-button').dataset.json);
+        displayPaperTitles(jsonData.nodes, event.detail.sortByCitations);
+    });
+
+    function displayPaperTitles(nodes, sortByCitations = true) {
+        // Trier les nœuds par nombre de citations ou date de publication
+        if (sortByCitations) {
+            nodes.sort((a, b) => b.citationCount - a.citationCount);
+        } else {
+            nodes.sort((a, b) => new Date(b.publicationDate) - new Date(a.publicationDate));
+        }
 
         titleList.innerHTML = ''; // Clear previous list
         nodes.forEach(node => {
